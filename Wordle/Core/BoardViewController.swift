acd2d52e1dad6635f8ec2d0d639a15d093df037f
Rudolf Amirian <ronaldo679@gmail.com>
2023-10-25 18:06:49 +0300

    VC for game

--- Wordle/Core/BoardViewController.swift
+++ Wordle/Core/BoardViewController.swift
@@ -7,23 +7,98 @@
 
 import UIKit
 
-class BoardViewController: UIViewController {
+protocol BoardViewControllerDataSource: AnyObject {
+    var currentGuesses: [[Character?]] {get}
+    
+    func boxColor(at indexPath: IndexPath) -> UIColor?
+}
 
+class BoardViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    
+    
+    weak var dataSource: BoardViewControllerDataSource?
+    
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumInteritemSpacing = 4
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        collectionView.backgroundColor = .clear
+        collectionView.register(KeyCell.self, forCellWithReuseIdentifier: KeyCell.identifier)
+        return collectionView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        view.addSubview(collectionView)
+        NSLayoutConstraint.activate([
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 30),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+        
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    public func reloadData() {
+        collectionView.reloadData()
     }
-    */
+    
+}
 
+extension BoardViewController {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return dataSource?.currentGuesses.count ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        let guesses = dataSource?.currentGuesses ?? []
+        return guesses[section].count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: KeyCell.identifier, for: indexPath) as? KeyCell else {
+            fatalError()
+        }
+        
+        
+        cell.backgroundColor = dataSource?.boxColor(at: indexPath)
+        cell.layer.borderWidth = 1
+        cell.layer.borderColor = UIColor.systemGray3.cgColor
+        
+        let guesses = dataSource?.currentGuesses ?? []
+        if let letter = guesses[indexPath.section][indexPath.row] {
+            cell.configure(with: letter)
+        }
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let margin: CGFloat = 20
+        let size: CGFloat = (collectionView.frame.size.width - margin) / 5
+        
+        return CGSize(width: size, height: size)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        
+        return UIEdgeInsets(top: 2,
+                            left: 2,
+                            bottom: 2,
+                            right: 2)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        //
+    }
 }
--- Wordle/Core/KeyCell.swift
+++ Wordle/Core/KeyCell.swift
@@ -8,5 +8,39 @@
 import UIKit
 
 class KeyCell: UICollectionViewCell {
+    static let identifier = "KeyCell"
     
+    let label: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .white
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 18, weight: .medium)
+        
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .systemGray5
+        contentView.addSubview(label)
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            label.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            label.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            label.topAnchor.constraint(equalTo: contentView.topAnchor),
+        ])
+    }
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        label.text = nil
+    }
+    
+    func configure(with letter: Character) {
+        label.text = String(letter).uppercased()
+    }
 }
